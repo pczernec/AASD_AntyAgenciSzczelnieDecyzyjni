6d75669eda8399c888de94ba5997ba145c04cdde
Wiktor Łazarski <wjlazarski@gmail.com>
2022-12-19 22:25:09 +0100

    Apply `black` and `isort` on the source code.

--- packages/agents/spade_agents/main.py
+++ packages/agents/spade_agents/main.py
@@ -2,18 +2,16 @@ import os
 import time
 
 from spade import quit_spade
-
 from spade_agents import SmartWatchAgent
 
 
 def main():
-    name = os.environ['AGENT_NAME']
-    password = os.environ['AGENT_PASSWORD']
-    xmpp = os.environ['XMPP']
+    name = os.environ["AGENT_NAME"]
+    password = os.environ["AGENT_PASSWORD"]
+    xmpp = os.environ["XMPP"]
 
     agent = SmartWatchAgent(f"{name}@{xmpp}", password)
     agent.start().result()
-
 
     while True:
         time.sleep(1)
